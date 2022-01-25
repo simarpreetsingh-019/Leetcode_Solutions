c37989a54b452056832b7d1653ccdd93e3d284df
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-01-26 00:00:23 +0530

    Time: 1 ms (60.53%), Space: 38.8 MB (41.57%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,16 +1,52 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i< index.length; i++)
-        {
-            list.add(index[i], nums[i]);
-        }
-        int[] res= new int[index.length];
-        for(int i = 0; i<index.length; i++)
-        {
-            res[i] = list.get(i);
-        }
-        return res;
+        int idx;
+int val;
+
+    int[] res = new int[nums.length];
+    for(int i=0,j=0 ;i<index.length&j<nums.length;i++,j++)
+    {
+      idx=index[i];  
+      val=nums[j];
+      InserRes(res,idx,val);
         
-    }
+    } 
+    return res;
+    
 }
+
+static void InserRes(int[] res, int index ,int value)
+{ 
+  
+  if(res[index]==-1)
+    {
+        res[index]=value;
+    }
+    else
+    {
+        int temp;
+        for(int i=index ; i<res.length;i++)
+        {
+            temp=res[i];
+            res[i]=value;
+            value=temp;
+        }
+        
+    } 
+}
+}
+//         List<Integer> list = new ArrayList<>();
+//         for(int i = 0; i< index.length; i++)
+//         {
+//             list.add(index[i], nums[i]);
+//         }
+//         int[] res= new int[index.length];
+//         for(int i = 0; i<index.length; i++)
+//         {
+//             res[i] = list.get(i);
+//         }
+//         return res;
+        
+//     }
+// }
+ 

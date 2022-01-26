7f7db1e98c22313869262fb43c9ca894a8732d3b
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-01-26 23:59:19 +0530

    Time: 3 ms (39.35%), Space: 39.1 MB (11.19%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-    char[] temp = new char[sentence.length()];
-    temp = sentence.toCharArray();
-    int[] freq = new int[26];
-    for(int i =0;i<temp.length;i++)
-    {
-        freq[temp[i] - 'a']++;
-    }
-    boolean res ;
-    for(int i : freq)
-    {
-        
-        if(i == 0)
-            return false;
-    }
-    return true;
-  }
-}    
+
+        boolean[] alphabet = new boolean[26];
     
+    for (int i = 0; i < sentence.length(); i++)
+    {
+		// [char-'a'] will get the index of array from 0 to 25
+		// Example: 'a' - 'a' == 0, 'b' - 'a' == 1, 'c' - 'a' == 2 and etc. 
+        if (!alphabet[sentence.charAt(i)-'a']) {
+            alphabet[sentence.charAt(i)-'a'] = true;
+        }
+    }
+    
+    for (boolean character : alphabet) {
+        if (!character) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+}

6074989637cd71c32ee2cd153febc8d556f82cdd
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-01-27 00:00:24 +0530

    Time: 2 ms (62.20%), Space: 38.7 MB (43.45%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,22 +1,18 @@
 class Solution {
-    public boolean checkIfPangram(String sentence) {
-
-        boolean[] alphabet = new boolean[26];
+ public boolean checkIfPangram(String sentence) {
+    int[] charArr = new int[26];
+    /**Iterate the sentence on character basis and store the count of occurence in array.
+    index is calculated as the difference between the ASCII value of current character and 'a' which will           be in between 0-25.
+    */
+    for(int i = 0 ; i < sentence.length() ; i++)
+        charArr[sentence.charAt(i)-'a']++;
     
-    for (int i = 0; i < sentence.length(); i++)
-    {
-		// [char-'a'] will get the index of array from 0 to 25
-		// Example: 'a' - 'a' == 0, 'b' - 'a' == 1, 'c' - 'a' == 2 and etc. 
-        if (!alphabet[sentence.charAt(i)-'a']) {
-            alphabet[sentence.charAt(i)-'a'] = true;
-        }
-    }
-    
-    for (boolean character : alphabet) {
-        if (!character) {
+    /**
+    Iterate the array again and check for any index where the frequency is 0. If any index has frequency 0          return false as it is not a pangram
+    */
+    for(int i = 0 ; i < charArr.length ; i++)
+        if(charArr[i]==0)
             return false;
-        }
-    }
     
     return true;
 }

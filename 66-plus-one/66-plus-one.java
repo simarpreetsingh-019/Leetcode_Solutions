f6c4e27d8111016ce70b8706a14ff3cbc539da1a
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-02-02 00:04:04 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (5.32%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int index = digits.length-1;
-        int carry = 1;
-    while(index>=0){
-            int sum = digits[index] + carry;
-            digits[index] = sum % 10;
-            carry = sum / 10;
-            index--;
-        }
-        
-        if(carry==0){
-            return digits;
-        } else {
-            int[] results = new int[digits.length+1];
-            results[0] = carry;
-            for(int i=1; i<results.length; i++){
-                results[i] = digits[i-1];
+        for(int i = digits.length - 1; i>= 0; i--)
+        {
+            if(digits[i] < 9)
+            {
+                digits[i]+=1;
+                return digits;
             }
-            return results;
+            digits[i] = 0;
         }
+        int[] new_ans = new int[digits.length+1];
+        new_ans[0] = 1;
+        return new_ans;
     }
 }

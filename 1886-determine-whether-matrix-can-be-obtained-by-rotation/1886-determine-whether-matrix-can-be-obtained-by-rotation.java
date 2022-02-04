56cf68cb0238739c8acb0c54843f65f57fcb3137
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-02-04 23:58:28 +0530

    Time: 1 ms (72.25%), Space: 42.1 MB (10.28%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,25 +1,33 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        for (int i = 0; i < 4; ++i) { // rotate 0, 1, 2, 3 times.
-            if (Arrays.deepEquals(mat, target)) {
-                return true;
+        int n = mat.length;
+        boolean rotate0 = true, rotate90 = true, rotate180 = true, rotate270 = true;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 0
+                if (mat[i][j] != target[i][j]) {
+                    rotate0 = false;
+                }
+                // 90
+                if (mat[i][j] != target[n - 1 - j][i]) {
+                    rotate90 = false;
+                }
+                // 180
+                if (mat[i][j] != target[n - 1 - i][n - 1 - j]) {
+                    rotate180 = false;
+                }
+                // 270
+                if (mat[i][j] != target[j][n - 1 - i]) {
+                    rotate270 = false;
+                }
+                
+                if (!rotate0 && !rotate90 && !rotate180 && !rotate270) {
+                    return false;
+                }
             }
-            rotate(mat);
         }
-        return false;
-    }
-    private void rotate(int[][] mat) {
-        for (int i = 0, j = mat.length - 1; i < j; ++i, --j) { // reverse rows order.
-            int[] tmp = mat[i];
-            mat[i] = mat[j];
-            mat[j] = tmp;
-        } 
-        for (int r = 0, R = mat.length; r < R; ++r) { // transpose.
-            for (int c = r + 1; c < R; ++c) {
-                int tmp = mat[r][c];
-                mat[r][c] = mat[c][r];
-                mat[c][r] = tmp;
-            }
-        }
+        
+        return true;
     }
 }

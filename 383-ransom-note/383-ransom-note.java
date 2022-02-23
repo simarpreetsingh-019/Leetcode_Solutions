6fc73e761e3c355678978b0ffcf562e8ec485cba
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-02-23 23:38:36 +0530

    Time: 9 ms (48.48%), Space: 42.8 MB (60.23%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -13,21 +13,38 @@
 //     }
 // }
 
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         int[] charArr1 = new int[26];
+//         int[] charArr2 = new int[26];
+
+//         for(int i=0;i<ransomNote.length();i++)
+//             charArr1[ransomNote.charAt(i)-'a']++;
+			
+//         for(int i=0;i<magazine.length();i++)
+//             charArr2[magazine.charAt(i)-'a']++;
+			
+//         for(int i=0;i<26;i++)
+//             if(charArr1[i]>charArr2[i])
+//                 return false;
+				
+//         return true;
+//     }
+// }
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] charArr1 = new int[26];
-        int[] charArr2 = new int[26];
-
-        for(int i=0;i<ransomNote.length();i++)
-            charArr1[ransomNote.charAt(i)-'a']++;
-			
-        for(int i=0;i<magazine.length();i++)
-            charArr2[magazine.charAt(i)-'a']++;
-			
-        for(int i=0;i<26;i++)
-            if(charArr1[i]>charArr2[i])
-                return false;
-				
-        return true;
+        HashMap<Character,Integer>  map = new HashMap();
+        
+        for(char ch1 : magazine.toCharArray()){
+            map.put(ch1,map.getOrDefault(ch1,0)+1);
+        }
+        int flag=0;
+        for(char ch2 : ransomNote.toCharArray()){
+            if(map.containsKey(ch2) && map.get(ch2)>0){
+                  flag++;
+                  map.put(ch2,map.get(ch2)-1);
+            }
+        }
+        return flag==ransomNote.length();
     }
 }

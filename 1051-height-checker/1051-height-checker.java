4b893f28e2dc39f9ee8c274dd7be3a5dfc07ca1d
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-01 00:09:22 +0530

    Time: 1 ms (94.01%), Space: 39.6 MB (48.84%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,17 +1,12 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] cnt = new int[101];
-        for (int h : heights) { 
-            cnt[h]++; // 3 1 1 1
-        }
-        for (int i = 1; i <= 100; i++) { 
-            cnt[i] += cnt[i - 1]; // 3 4 5 6
-        }
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
         int ans = 0;
-        for (int i = heights.length - 1; i >= 0; i--) {
-            if (heights[--cnt[heights[i]]] != heights[i]) 
+        for (int i = 0; i < expected.length; ++i) {
+            if (expected[i] != heights[i]) 
                 ans++;
-        }   
+        }
         return ans;
     }
 }

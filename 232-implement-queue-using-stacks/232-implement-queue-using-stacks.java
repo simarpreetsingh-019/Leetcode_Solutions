d574bd46d56c71ea8e9bc3390a72e7f5bc67ba48
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-02 23:07:00 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (50.26%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,51 +1,85 @@
-class MyQueue {
+// class MyQueue {
     
-    private Stack<Integer> first = new Stack<>();
-    private Stack<Integer> second = new Stack<>();
+//     private Stack<Integer> first = new Stack<>();
+//     private Stack<Integer> second = new Stack<>();
     
-    public MyQueue() {
+//     public MyQueue() {
         
-    }
+//     }
     
 
 
-    public void push(int x) 
-    {
-        first.push(x);
-        move();
+//     public void push(int x) 
+//     {
+//         first.push(x);
+//         move();
+//     }
+    
+//     public int pop()
+//     {        
+//         move();
+//         return second.pop();
+//     }
+    
+//     public int peek()
+//     {
+//         move();
+//         return second.peek();
+//     }
+    
+//     public boolean empty() {
+//         return first.isEmpty() && second.isEmpty();
+//     }
+    
+//     private void move() {
+//         if (!second.isEmpty()) {
+//             return;
+//         }
+//         while(!first.isEmpty()) {
+//             second.push(first.pop());
+//         }
+//     }
+// }
+
+// /**
+//  * Your MyQueue object will be instantiated and called as such:
+//  * MyQueue obj = new MyQueue();
+//  * obj.push(x);
+//  * int param_2 = obj.pop();
+//  * int param_3 = obj.peek();
+//  * boolean param_4 = obj.empty();
+//  */
+
+class MyQueue {
+    private Stack<Integer> stack;
+    public MyQueue() {
+        stack=new Stack<Integer>();
     }
     
-    public int pop()
-    {        
-        move();
-        return second.pop();
+    public void push(int x){
+        stack.push(x);
     }
     
-    public int peek()
-    {
-        move();
-        return second.peek();
+    public int pop() {
+        int front=stack.firstElement();
+        Stack<Integer> backup=new Stack<Integer>();
+        while(!stack.empty())
+        {
+            backup.push(stack.pop());
+        }
+        backup.pop();
+        while(!backup.empty())
+        {
+            stack.push(backup.pop());
+        }
+        return front;
+    }
+    
+    public int peek() {
+        return stack.firstElement();
     }
     
     public boolean empty() {
-        return first.isEmpty() && second.isEmpty();
-    }
-    
-    private void move() {
-        if (!second.isEmpty()) {
-            return;
-        }
-        while(!first.isEmpty()) {
-            second.push(first.pop());
-        }
+        return stack.empty();
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */

ffa01a0e811f7fee6183fa634faf284a13b4d00f
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-03 23:39:46 +0530

    Time: 23 ms (58.53%), Space: 67 MB (13.91%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -1,18 +1,34 @@
 class Solution {
-    public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List<List<Integer>> ans = new ArrayList();
+    public List<List<Integer>> minimumAbsDifference(final int[] arr) {
         Arrays.sort(arr);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length - 1; i++) {
-            int diff = arr[i + 1] - arr[i];
-            if (diff < min) {
-                min = diff;
-                ans.clear();  // ans = new ArrayList<>();
-                ans.add(Arrays.asList(arr[i], arr[i + 1]));
-            } else if (diff == min) {
-                ans.add(Arrays.asList(arr[i], arr[i + 1]));
+        final List<List<Integer>> result = new LinkedList<>();
+        int minDistance = Integer.MAX_VALUE;
+        for (int i=1; i<arr.length; i++) {
+            minDistance = Math.min(minDistance, arr[i] - arr[i-1]);
+        }
+        for (int i=1; i<arr.length; i++) {
+            if (minDistance == arr[i] - arr[i - 1]) {
+                result.add(Arrays.asList(arr[i-1], arr[i]));
             }
         }
-        return ans;
+        return result;
     }
 }
+// class Solution {
+//     public List<List<Integer>> minimumAbsDifference(int[] arr) {
+//         List<List<Integer>> ans = new ArrayList();
+//         Arrays.sort(arr);
+//         int min = Integer.MAX_VALUE;
+//         for (int i = 0; i < arr.length - 1; i++) {
+//             int diff = arr[i + 1] - arr[i];
+//             if (diff < min) {
+//                 min = diff;
+//                 ans.clear();  // ans = new ArrayList<>();
+//                 ans.add(Arrays.asList(arr[i], arr[i + 1]));
+//             } else if (diff == min) {
+//                 ans.add(Arrays.asList(arr[i], arr[i + 1]));
+//             }
+//         }
+//         return ans;
+//     }
+// }

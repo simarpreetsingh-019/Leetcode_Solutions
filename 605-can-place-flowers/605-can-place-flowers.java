0b3187dd789d99f603dd3cadce479d01d4ca2c5c
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-17 23:31:22 +0530

    Time: 1 ms (96.46%), Space: 43 MB (78.90%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.java
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -1,15 +1,34 @@
+// class Solution {
+// public boolean canPlaceFlowers(int[] flowerbed, int n) {
+//     int count = 1;
+//     int result = 0;
+//     for(int i=0; i<flowerbed.length; i++) {
+//         if(flowerbed[i] == 0) {
+//             count++;
+//         }else {
+//             result += (count-1)/2; // 1 will be added in result only if count=3, means 3 continuous 0. 2 will be added for count=5, 3 for count=7 and so on.
+//             count = 0;
+//         }
+//     }
+//     if(count != 0) result += count/2;
+//     return result>=n;
+// }}
+
 class Solution {
-public boolean canPlaceFlowers(int[] flowerbed, int n) {
-    int count = 1;
-    int result = 0;
-    for(int i=0; i<flowerbed.length; i++) {
-        if(flowerbed[i] == 0) {
-            count++;
-        }else {
-            result += (count-1)/2; // 1 will be added in result only if count=3, means 3 continuous 0. 2 will be added for count=5, 3 for count=7 and so on.
-            count = 0;
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for(int i = 0; i < flowerbed.length && count < n; i++) {
+            if(flowerbed[i] == 0) {
+	            //get next and prev flower bed slot values. If i lies at the ends the next and prev are considered as 0. 
+               int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1]; 
+               int prev = (i == 0) ? 0 : flowerbed[i - 1];
+               if(next == 0 && prev == 0) {
+                   flowerbed[i] = 1;
+                   count++;
+               }
+            }
         }
+        
+        return count == n;
     }
-    if(count != 0) result += count/2;
-    return result>=n;
-}}
+}

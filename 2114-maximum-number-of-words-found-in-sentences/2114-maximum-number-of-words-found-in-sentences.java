c409e6f7187f325759f0e72949ed7a88c17b8c80
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-20 00:02:21 +0530

    Time: 6 ms (49.81%), Space: 45.3 MB (24.77%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,21 +1,21 @@
-class Solution {
-    public int mostWordsFound(String[] sentences) {
-        int max = Integer.MIN_VALUE;
-        for (String sentence : sentences) {
-            max = Math.max(max, sentence.split(" ").length);
-        }
-        return max;
-    }
-}
-
 // class Solution {
 //     public int mostWordsFound(String[] sentences) {
-//         int max = 0;
-//         for (String s: sentences) {
-//             // int a = s.split(" ").length;
-//             // max = max > a ? max : a;
-//             max = Math.max(max, s.split(" ").length); // Alternative of above 2 lines
+//         int max = Integer.MIN_VALUE;
+//         for (String sentence : sentences) {
+//             max = Math.max(max, sentence.split(" ").length);
 //         }
 //         return max;
 //     }
 // }
+
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
+        for (String s: sentences) {
+            // int a = s.split(" ").length;
+            // max = max > a ? max : a;
+            max = Math.max(max, s.split(" ").length); // Alternative of above 2 lines
+        }
+        return max;
+    }
+}

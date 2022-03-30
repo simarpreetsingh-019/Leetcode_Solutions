40a471174987aef6444ff1ab0e07108621a0e1ed
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-03-30 23:57:52 +0530

    Time: 3 ms (66.41%), Space: 42.1 MB (38.15%) - LeetHub

---
+++ 728-self-dividing-numbers/728-self-dividing-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = left, j = 0; i <= right; i++) {
+            for (j = i; j > 0; j /= 10)
+                if (j % 10 == 0 || i % (j % 10) != 0) break;
+            if (j == 0) res.add(i);
+        }
+        return res;
+    }
+}

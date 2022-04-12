23c25ce453f2fac8f1ab8048a02dc63923397328
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-04-12 23:59:58 +0530

    Time: 25 ms (56.80%), Space: 72.6 MB (31.76%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,32 +15,16 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
-        
-        List<Integer> res = new ArrayList<>();
-        
-        while(root1 != null || root2 != null || !st1.empty() || !st2.empty()){
-            while(root1 != null){
-                st1.push(root1);
-                root1 = root1.left;
-            }
-            while(root2 != null){
-                st2.push(root2);
-                root2 = root2.left;
-            }
-            if(st2.empty() || (!st1.empty() && st1.peek().val <= st2.peek().val)){
-                root1 = st1.pop();
-                res.add(root1.val);
-                root1 = root1.right;
-            }
-            else{
-                root2 = st2.pop();
-                res.add(root2.val);
-                root2 = root2.right;
-            }
-        }
-        return res;
+        List<Integer> ans = new ArrayList<>();
+        inorder(root1, ans);
+        inorder(root2, ans);
+        Collections.sort(ans); // Here it calls List.sort(object) method.
+        return ans;
+    }
+    private void inorder(TreeNode n, List<Integer> ans) {
+        if (n == null) return;
+        inorder(n.left, ans);
+        ans.add(n.val);
+        inorder(n.right, ans);
     }
 }
-

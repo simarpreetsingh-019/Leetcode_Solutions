74724e9115fc1cea1f2c5141518368745fed5f94
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-04-12 23:59:45 +0530

    Time: 51 ms (14.36%), Space: 68.1 MB (77.35%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,23 +15,32 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        Queue<Integer> q1 = new LinkedList<>(), q2 = new LinkedList<>();
-        inorder(root1, q1);
-        inorder(root2, q2);
-        List<Integer> ans = new ArrayList<>();
-        while (!q1.isEmpty() || !q2.isEmpty())
-            if (q2.isEmpty())
-                ans.add(q1.poll());
-            else if (q1.isEmpty())
-                ans.add(q2.poll());
-            else
-                ans.add(q1.peek() < q2.peek() ? q1.poll() : q2.poll());
-        return ans;
-    }
-    private void inorder(TreeNode n, Queue<Integer> q) {
-        if (n == null) return;
-        inorder(n.left, q);
-        q.offer(n.val);
-        inorder(n.right, q);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        
+        List<Integer> res = new ArrayList<>();
+        
+        while(root1 != null || root2 != null || !st1.empty() || !st2.empty()){
+            while(root1 != null){
+                st1.push(root1);
+                root1 = root1.left;
+            }
+            while(root2 != null){
+                st2.push(root2);
+                root2 = root2.left;
+            }
+            if(st2.empty() || (!st1.empty() && st1.peek().val <= st2.peek().val)){
+                root1 = st1.pop();
+                res.add(root1.val);
+                root1 = root1.right;
+            }
+            else{
+                root2 = st2.pop();
+                res.add(root2.val);
+                root2 = root2.right;
+            }
+        }
+        return res;
     }
 }
+

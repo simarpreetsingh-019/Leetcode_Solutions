d784ce8b1242b0d84c13ab2465d4ade59b1af828
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-04-12 23:59:16 +0530

    Time: 30 ms (42.91%), Space: 73.8 MB (10.80%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,16 +15,23 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Queue<Integer> q1 = new LinkedList<>(), q2 = new LinkedList<>();
+        inorder(root1, q1);
+        inorder(root2, q2);
         List<Integer> ans = new ArrayList<>();
-        inorder(root1, ans);
-        inorder(root2, ans);
-        Collections.sort(ans); // Here it calls List.sort(object) method.
+        while (!q1.isEmpty() || !q2.isEmpty())
+            if (q2.isEmpty())
+                ans.add(q1.poll());
+            else if (q1.isEmpty())
+                ans.add(q2.poll());
+            else
+                ans.add(q1.peek() < q2.peek() ? q1.poll() : q2.poll());
         return ans;
     }
-    private void inorder(TreeNode n, List<Integer> ans) {
+    private void inorder(TreeNode n, Queue<Integer> q) {
         if (n == null) return;
-        inorder(n.left, ans);
-        ans.add(n.val);
-        inorder(n.right, ans);
+        inorder(n.left, q);
+        q.offer(n.val);
+        inorder(n.right, q);
     }
 }

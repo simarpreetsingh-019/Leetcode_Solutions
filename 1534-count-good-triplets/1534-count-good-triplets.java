f1d239142d5b801de9e368c554632dad3fe91374
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-04-20 23:45:51 +0530

    Time: 17 ms (62.88%), Space: 39.7 MB (95.93%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -1,13 +1,11 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int count = 0;
-        int n = arr.length;
-        for(int i = 0; i < n -2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                if(Math.abs(arr[i] - arr[j]) <= a) { // check if satisfy then loop for k
-                    for(int k = j + 1; k < n; k++) {
-                        if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c)
-                            count++;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    if( Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c){
+                        count++;
                     }
                 }
             }
@@ -15,3 +13,20 @@ class Solution {
         return count;
     }
 }
+// class Solution {
+//     public int countGoodTriplets(int[] arr, int a, int b, int c) {
+//         int count = 0;
+//         int n = arr.length;
+//         for(int i = 0; i < n -2; i++) {
+//             for(int j = i + 1; j < n - 1; j++) {
+//                 if(Math.abs(arr[i] - arr[j]) <= a) { // check if satisfy then loop for k
+//                     for(int k = j + 1; k < n; k++) {
+//                         if(Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c)
+//                             count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }

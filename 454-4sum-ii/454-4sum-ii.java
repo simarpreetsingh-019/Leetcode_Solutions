5ab638914224bf0e6f66ef9738f6e498c88ea39b
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-05-14 23:59:01 +0530

    Time: 134 ms (84.37%), Space: 42.4 MB (83.14%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int c : nums3)
+            for(int d : nums4)
+                map.put(c+d, map.getOrDefault(c+d,0)+1);
+        int ans=0;
+        for(int a : nums1)
+            for(int b : nums2)
+                ans += map.getOrDefault(-(a+b),0);
+        return ans;
+    }
+}

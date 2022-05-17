dc46a078bec9f3b506912e2efe9d6eb99d99ac1a
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-05-18 03:58:24 +0530

    Time: 2 ms (75.83%), Space: 104.3 MB (48.23%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -12,11 +12,9 @@ class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
         ListNode L1 = list1;
         int cnt=1;
-        for(; cnt<a && list1.next != null; cnt++){
-            list1 = list1.next;
-        }
-        ListNode S = list1;
+        ListNode S = list1; // take any value because it will be changed in if condition
         for(; cnt<=b && list1.next != null; cnt++){
+            if(cnt == a) S = list1;
             list1 = list1.next;
         }
         S.next = list2;

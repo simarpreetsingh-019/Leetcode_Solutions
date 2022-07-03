20218e556032cc8e40a3a8e1910bf4c11083c3d6
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-07-03 22:39:17 +0530

    Time: 4 ms (38.42%), Space: 43.3 MB (61.45%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         for(int i  = 0 ; i<arr.length;i++)
+//         {
+//             for (int j = 0; j< arr.length; j++)
+//             {
+//                 if(arr[i] == arr[j]*2 && i != j)
+//                     return true;
+                    
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i  = 0 ; i<arr.length;i++)
-        {
-            for (int j = 0; j< arr.length; j++)
-            {
-                if(arr[i] == arr[j]*2 && i != j)
-                    return true;
-                    
-            }
+        HashSet<Integer> set = new HashSet<>();
+        for(int a : arr) {
+            if(set.contains(a*2) || (a%2 == 0 && set.contains(a/2))) 
+                return true;
+            set.add(a);
         }
         return false;
     }

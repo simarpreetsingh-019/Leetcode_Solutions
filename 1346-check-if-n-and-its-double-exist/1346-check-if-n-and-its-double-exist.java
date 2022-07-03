58369ac880f2a39a622ec7d014ba7577040b997c
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-07-03 22:39:34 +0530

    Time: 3 ms (66.14%), Space: 44.1 MB (17.40%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,26 +1,26 @@
-// class Solution {
-//     public boolean checkIfExist(int[] arr) {
-//         for(int i  = 0 ; i<arr.length;i++)
-//         {
-//             for (int j = 0; j< arr.length; j++)
-//             {
-//                 if(arr[i] == arr[j]*2 && i != j)
-//                     return true;
-                    
-//             }
-//         }
-//         return false;
-//     }
-// }
-
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int a : arr) {
-            if(set.contains(a*2) || (a%2 == 0 && set.contains(a/2))) 
-                return true;
-            set.add(a);
+        for(int i  = 0 ; i<arr.length;i++)
+        {
+            for (int j = 0; j< arr.length; j++)
+            {
+                if(arr[i] == arr[j]*2 && i != j)
+                    return true;
+                    
+            }
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int a : arr) {
+//             if(set.contains(a*2) || (a%2 == 0 && set.contains(a/2))) 
+//                 return true;
+//             set.add(a);
+//         }
+//         return false;
+//     }
+// }

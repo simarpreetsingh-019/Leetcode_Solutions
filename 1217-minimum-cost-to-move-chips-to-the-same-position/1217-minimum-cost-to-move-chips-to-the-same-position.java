f26330d5859d15a25fd6cd9c3c146ddce484d5e3
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-08-23 23:24:35 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (35.65%) - LeetHub

---
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd=0, even=0;
+        for(int i : position){
+            if(i%2==0) even++;
+            else odd++;
+        }
+        return Math.min(even, odd);
+    }
+}

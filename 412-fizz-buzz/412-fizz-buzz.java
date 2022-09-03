c5c30cf59ed6a91060978e5500b2a8e0f9542904
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-09-03 23:45:11 +0530

    Time: 3 ms (33.78%), Space: 48.1 MB (66.80%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> L = new LinkedList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3==0 && i%5==0) L.add("FizzBuzz");
+            else if(i%3==0) L.add("Fizz");
+            else if(i%5==0) L.add("Buzz");
+            else L.add(i+"");
+        }
+        return L;
+    }
+}

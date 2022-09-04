8dfcfbeabaf6079ef340a5403cff276fad136879
Simarpreet Singh <simarpreetsingh.019@gmail.com>
2022-09-04 23:51:39 +0530

    Time: 11 ms (36.27%), Space: 43.8 MB (61.28%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack();
+        for(String cur: path.split("/")){
+            if(cur.equals("..")) {
+                if(!stack.empty()) stack.pop();
+            }
+            else if(cur.length()>0 && !cur.equals(".")) stack.push(cur);
+        }
+        return "/"+String.join("/",stack);
+    }
+}
